40c23889bc6752af0014ac559f6f3511373f76f2
HUGORDdev <juniordita78@gmail.com>
2025-12-15 16:03:07 +0100

    la versio 1.o est deja prete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const menuOpenButton = document.querySelector('#menu-open-button');
+const navlinks = document.querySelector('.nav-menu .nac-links');
 const menuCloseButton = document.querySelector('#menu-close-button');
 
 // open menu when clicking the open button
@@ -10,3 +11,43 @@ menuOpenButton.addEventListener('click', () => {
 menuCloseButton.addEventListener('click', () => {
     document.body.classList.remove('show-mobile-menu');
 })
+navlinks.forEach(link => {
+  link.addEventListener('click',()=>menuOpenButton.click)
+  
+});()
+
+// intialise swipper 
+const swiper = new Swiper('.slider-wrapper', {
+  // Optional parameters
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+//   responsive breakpoints
+  breakpoints: {
+    O:{
+        slidesPerView: 1,
+    },
+    768:{
+        slidesPerView: 2,
+    },
+    1024:{
+        slidesPerView: 3,
+    }
+  }
+  
+});
+
+
